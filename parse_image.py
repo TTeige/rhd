0818e153bb7c140219f4ad59e4c70517c69f931b
tteige <tim.teige91@gmail.com>
2017-11-09 11:06:28 +0100

    cleaned up

--- parse_image.py
+++ parse_image.py
@@ -28,101 +28,6 @@ def truncate_float(f, n):
     return '.'.join([i, (d + '0' * n)[:n]])
 
 
-# def find_if_close(cnt1, cnt2):
-#     row1, row2 = cnt1.shape[0], cnt2.shape[0]
-#     for i in range(row1):
-#         for j in range(row2):
-#             dist = np.linalg.norm(cnt1[i] - cnt2[j])
-#             if abs(dist) < 2:
-#                 return True
-#             elif i == row1 - 1 and j == row2 - 1:
-#                 return False
-#
-#
-# def non_max_suppression_fast(boxes, overlapThresh):
-#     # if there are no boxes, return an empty list
-#     if len(boxes) == 0:
-#         return []
-#
-#     # if the bounding boxes integers, convert them to floats --
-#     # this is important since we'll be doing a bunch of divisions
-#     if boxes.dtype.kind == "i":
-#         boxes = boxes.astype("float")
-#
-#     # initialize the list of picked indexes
-#     pick = []
-#
-#     # grab the coordinates of the bounding boxes
-#     x1 = boxes[:, 0]
-#     y1 = boxes[:, 1]
-#     x2 = boxes[:, 2]
-#     y2 = boxes[:, 3]
-#
-#     # compute the area of the bounding boxes and sort the bounding
-#     # boxes by the bottom-right y-coordinate of the bounding box
-#     area = (x2 - x1 + 1) * (y2 - y1 + 1)
-#     idxs = np.argsort(y2)
-#
-#     # keep looping while some indexes still remain in the indexes
-#     # list
-#     while len(idxs) > 0:
-#         # grab the last index in the indexes list and add the
-#         # index value to the list of picked indexes
-#         last = len(idxs) - 1
-#         i = idxs[last]
-#         pick.append(i)
-#
-#         # find the largest (x, y) coordinates for the start of
-#         # the bounding box and the smallest (x, y) coordinates
-#         # for the end of the bounding box
-#         xx1 = np.maximum(x1[i], x1[idxs[:last]])
-#         yy1 = np.maximum(y1[i], y1[idxs[:last]])
-#         xx2 = np.minimum(x2[i], x2[idxs[:last]])
-#         yy2 = np.minimum(y2[i], y2[idxs[:last]])
-#
-#         # compute the width and height of the bounding box
-#         w = np.maximum(0, xx2 - xx1 + 1)
-#         h = np.maximum(0, yy2 - yy1 + 1)
-#
-#         # compute the ratio of overlap
-#         overlap = (w * h) / area[idxs[:last]]
-#
-#         # delete all indexes from the index list that have
-#         idxs = np.delete(idxs, np.concatenate(([last],
-#                                                np.where(overlap > overlapThresh)[0])))
-#
-#     # return only the bounding boxes that were picked using the
-#     # integer data type
-#     return boxes[pick].astype("int")
-#
-#
-# def join_contours(contours):
-#     length = len(contours)
-#     status = np.zeros((length, 1))
-#     for i, cnt1 in enumerate(contours):
-#         x = 1
-#         if i != length - 2:
-#             for j, cnt2 in enumerate(contours[i + 1:]):
-#                 x = x + 1
-#                 dist = find_if_close(cnt1, cnt2)
-#                 if dist:
-#                     val = min(status[i], status[x])
-#                     status[x] = status[i] = val
-#                 else:
-#                     if status[x] == status[i]:
-#                         status[x] = i + 1
-#
-#     unified = []
-#     maximum = int(status.max()) + 1
-#     for i in range(maximum):
-#         pos = np.where(status == i)[0]
-#         if pos.size != 0:
-#             cont = np.vstack(contours[i] for i in pos)
-#             hull = cv2.convexHull(cont)
-#             unified.append(hull)
-#     return unified
-
-
 def handle_image(target_dir, file_location, fn):
     try:
         img_original = cv2.imread(file_location)
@@ -142,40 +47,14 @@ def handle_image(target_dir, file_location, fn):
         with open(os.path.join(single_image_dir, fn.split('.')[0] + "_bb_locations.csv"), "w") as csv_file:
             writer = csv.DictWriter(csv_file, ['fn', 'x', 'y', 'w', 'h'])
             writer.writeheader()
-            # bounding_rects = []
             for i, cnt in enumerate(contours):
                 x, y, w, h = cv2.boundingRect(cnt)
                 rect = Rect(x, y, w, h)
-                # bounding_rects.append(rect)
-
-                # completed_boxes = []
-
-                # Make a naive assumption  that rectangles can only be merged once
-                # Have to work around the edge cases of poorly written 5 numbers
-                # for rect in bounding_rects:
-                #     for rect2 in bounding_rects:
-                #         if rect == rect2:
-                #             continue
-                #         if check_merge(rect, rect2):
-                #             merged = merge_rects(rect, rect2)
-                #             completed_boxes.append(merged)
-                #             bounding_rects.remove(rect2)
-                #             bounding_rects.remove(rect)
-                #             break
-                #
-                # for rect in bounding_rects:
-                #     completed_boxes.append(rect)
-
-                # for i, rect in enumerate(completed_boxes):
-                # if not validate_box(rect.x, rect.y, rect.w, rect.h):
-                #     continue
                 candidate = extract_number(rect.x, rect.y, rect.w, rect.h, img)
                 reshaped = reshape_img(candidate)
                 fn_final = os.path.join(single_image_dir, (str(i) + "_" + fn))
-                writer.writerow({'fn': fn_final, 'x': int(rect.x / 0.3), 'y': int(rect.y / 0.3),
-                                 'w': int(rect.w / 0.3), 'h': int(rect.h / 0.3)})
-                # writer.writerow({'fn': fn_final, 'x': int(rect.x), 'y': int(rect.y),
-                #                  'w': int(rect.w), 'h': int(rect.h)})
+                writer.writerow({'fn': fn_final, 'x': int(rect.x), 'y': int(rect.y),
+                                 'w': int(rect.w), 'h': int(rect.h)})
                 cv2.imwrite(fn_final, reshaped)
         return fn
 
@@ -183,54 +62,10 @@ def handle_image(target_dir, file_location, fn):
         print(e)
 
 
-def merge_rects(rect1, rect2):
-    if rect1.x <= rect2.x:
-        new_x = rect1.x
-    else:
-        new_x = rect2.x
-
-    if rect1.y <= rect2.y:
-        new_y = rect1.y
-    else:
-        new_y = rect2.y
-
-    if rect1.x + rect1.w <= rect2.x + rect2.w:
-        new_x2 = rect2.x + rect2.w
-    else:
-        new_x2 = rect1.x + rect1.w
-
-    if rect1.y + rect1.h <= rect2.y + rect2.h:
-        new_y2 = rect2.y + rect2.h
-    else:
-        new_y2 = rect1.y + rect1.h
-
-    new_w = new_x2 - new_x
-    new_h = new_y2 - new_y
-
-    return Rect(new_x, new_y, new_w, new_h)
-
-
-def validate_box(x, y, w, h):
-    if w * h < 50 or w * h > 3000:
-        return False
-    if w / h > 5 or h / w > 5:
-        return False
-    return True
-
-
 def calculate_distance(rect1, rect2):
     a = np.array([rect1.x, rect1.y])
     b = np.array([rect2.x, rect2.y])
     return np.linalg.norm(a - b)
-
-
-def check_merge(rect1, rect2):
-    # Check if the merge target is between leftmost and rightmost edge of the bounding box
-    if rect1.x - 10 <= rect2.x <= rect1.x + rect1.w + 10:
-        # Only merge if box is above and within threshold
-        if np.abs(rect1.y - rect2.y) < 10 and rect2.h < 10:
-            return True
-    return False
 
 
 def extract_number(x, y, w, h, img):
