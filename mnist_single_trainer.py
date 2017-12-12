a83683e158f5fb50bf06aafc8aefe42ffea1dca0
tteige <tim.teige91@gmail.com>
2017-12-12 12:42:20 +0100

    Fixed the features generated by the convolution

--- mnist_single_trainer.py
+++ mnist_single_trainer.py
@@ -60,15 +60,15 @@ def convDeepnn(x):
     h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
     h_pool2 = max_pool_2x2(h_conv2)
 
-    W_fc1 = weight_variable([32 * 32 * 128, 1024])
+    W_fc1 = weight_variable([32 * 32 * 64, 1024])
     b_fc1 = bias_variable([1024])
 
-    h_pool2_flat = tf.reshape(h_pool2, [-1, 32 * 32 * 128])
+    h_pool2_flat = tf.reshape(h_pool2, [-1, 32 * 32 * 64])
     h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
     keep_prob = tf.placeholder(tf.float32)
     h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
-    W_fc2 = weight_variable([2048, 1001])
+    W_fc2 = weight_variable([1024, 1001])
     b_fc2 = bias_variable([1001])
 
     y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
