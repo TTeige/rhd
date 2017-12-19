064282f6a88d8e50e7c9d8756d5fe6aa7ba34e5d
tteige <tim.teige91@gmail.com>
2017-12-19 09:19:58 +0100

    created a class for the trainer instead of a single script

--- mnist_single_trainer.py
+++ mnist_single_trainer.py
@@ -5,6 +5,112 @@ import numpy as np
 import cv2
 
 
+class Trainer:
+    def __init__(self):
+
+        self.X_train = None
+        self.y_train = None
+        self.y_val = None
+        self.X_val = None
+        self.X_test = None
+        self.y_test = None
+        self.x = tf.placeholder(tf.float32, [None, 128, 128], name='x')
+        self.y_ = tf.placeholder(tf.int64, [None, 1001], name='y_')
+
+        self.batch_size = 50
+        self.predictor = None
+        self.predictor2 = None
+        self.y_conv = None
+        self.keep_prob = 1.0
+
+    def load_training_dataset(self, path='data/MNIST_synthetic.h5'):
+        h5f = h5py.File(path, 'r')
+
+        self.X_train = h5f['train_dataset'][:]
+        self.y_train = h5f['train_labels'][:]
+        self.y_val = h5f['valid_labels'][:]
+        self.X_val = h5f['valid_dataset'][:]
+        self.X_test = h5f['test_dataset'][:]
+        self.y_test = h5f['test_labels'][:]
+
+    def weight_variable(self, shape):
+        initial = tf.truncated_normal(shape, stddev=0.1)
+        return tf.Variable(initial)
+
+    def bias_variable(self, shape):
+        initial = tf.constant(0.1, shape=shape)
+        return tf.Variable(initial)
+
+    def conv2d(self, x, W):
+        return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+    def max_pool_2x2(self, x):
+        return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
+                              strides=[1, 2, 2, 1], padding='SAME')
+
+    def convDeepnn(self, x):
+        W_conv1 = self.weight_variable([5, 5, 1, 32])
+        b_conv1 = self.bias_variable([32])
+
+        x_image = tf.reshape(x, [-1, 128, 128, 1])
+
+        h_conv1 = tf.nn.relu(self.conv2d(x_image, W_conv1) + b_conv1)
+        h_pool1 = self.max_pool_2x2(h_conv1)
+
+        W_conv2 = self.weight_variable([5, 5, 32, 64])
+        b_conv2 = self.bias_variable([64])
+
+        h_conv2 = tf.nn.relu(self.conv2d(h_pool1, W_conv2) + b_conv2)
+        h_pool2 = self.max_pool_2x2(h_conv2)
+
+        W_fc1 = self.weight_variable([32 * 32 * 64, 1024])
+        b_fc1 = self.bias_variable([1024])
+
+        h_pool2_flat = tf.reshape(h_pool2, [-1, 32 * 32 * 64])
+        h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
+        keep_prob = tf.placeholder(tf.float32)
+        h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+        W_fc2 = self.weight_variable([1024, 1001])
+        b_fc2 = self.bias_variable([1001])
+
+        y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+
+        return y_conv, keep_prob
+
+    def run_training(self):
+
+        y_conv, keep_prob = self.convDeepnn(self.x)
+        self.load_training_dataset()
+
+        cross_entropy = tf.reduce_mean(
+            tf.nn.softmax_cross_entropy_with_logits(labels=self.y_, logits=y_conv))
+        train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+
+        correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(self.y_, 1), name='cor_pred')
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+        for i in range(200000):
+            offset = (i * self.batch_size) % (self.y_train.shape[0] - self.batch_size)
+            batch_data = self.X_train[offset:(offset + self.batch_size), :]
+            batch_labels = self.y_train[offset:(offset + self.batch_size), :]
+
+            if i % 100 == 0:
+                train_accuracy = accuracy.eval(feed_dict={
+                    self.x: batch_data, self.y_: batch_labels, keep_prob: 1.0})
+
+                print('step {}, training accuracy {}'.format(i, train_accuracy))
+
+            train_step.run(feed_dict={self.x: batch_data, self.y_: batch_labels, keep_prob: 0.5})
+
+        print('test accuracy %g' % accuracy.eval(feed_dict={
+            self.x: self.X_val[:1000], self.y_: self.y_val[:1000], keep_prob: 1.0}))
+
+    def initialize_tensors(self, y_conv):
+        self.predictor = tf.nn.softmax(y_conv, 1, name='predictor')
+        self.predictor2 = tf.argmax(y_conv, 1)
+
+
 def convert_img(img):
     img_data = cv2.bitwise_not(img)
     # img_data = np.array(img_data, dtype=np.float32)
@@ -26,113 +132,34 @@ def convert_img(img):
     return img_data
 
 
-def weight_variable(shape):
-    initial = tf.truncated_normal(shape, stddev=0.1)
-    return tf.Variable(initial)
-
-
-def bias_variable(shape):
-    initial = tf.constant(0.1, shape=shape)
-    return tf.Variable(initial)
-
-
-def conv2d(x, W):
-    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
-
-
-def max_pool_2x2(x):
-    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
-                          strides=[1, 2, 2, 1], padding='SAME')
-
-
-def convDeepnn(x):
-    W_conv1 = weight_variable([5, 5, 1, 32])
-    b_conv1 = bias_variable([32])
-
-    x_image = tf.reshape(x, [-1, 128, 128, 1])
-
-    h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-    h_pool1 = max_pool_2x2(h_conv1)
-
-    W_conv2 = weight_variable([5, 5, 32, 64])
-    b_conv2 = bias_variable([64])
-
-    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-    h_pool2 = max_pool_2x2(h_conv2)
-
-    W_fc1 = weight_variable([32 * 32 * 64, 1024])
-    b_fc1 = bias_variable([1024])
-
-    h_pool2_flat = tf.reshape(h_pool2, [-1, 32 * 32 * 64])
-    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
-
-    keep_prob = tf.placeholder(tf.float32)
-    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
-    W_fc2 = weight_variable([1024, 1001])
-    b_fc2 = bias_variable([1001])
-
-    y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
-
-    return y_conv, keep_prob
-
-
 def run(args):
-    x = tf.placeholder(tf.float32, [None, 128, 128], name='x')
-    y_ = tf.placeholder(tf.int64, [None, 1001], name='y_')
-    y_conv, keep_prob = convDeepnn(x)
-    cross_entropy = tf.reduce_mean(
-        tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
-
-    correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1), name='cor_pred')
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-
-    predictor = tf.nn.softmax(y_conv, 1, name='predictor')
-    predictor2 = tf.argmax(y_conv, 1)
+    trainer = Trainer()
     saver = tf.train.Saver()
 
-    h5f = h5py.File('data/MNIST_synthetic.h5', 'r')
-
-    X_train = h5f['train_dataset'][:]
-    y_train = h5f['train_labels'][:]
-    X_val = h5f['valid_dataset'][:]
-    y_val = h5f['valid_labels'][:]
-    X_test = h5f['test_dataset'][:]
-    y_test = h5f['test_labels'][:]
-
     if args.train:
-
-        train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-        batch_size = 50
 
         with tf.Session() as sess:
             sess.run(tf.global_variables_initializer())
             if tf.train.latest_checkpoint("model/"):
                 saver.restore(sess, tf.train.latest_checkpoint('model/'))
-            for i in range(200000):
 
-                offset = (i * batch_size) % (y_train.shape[0] - batch_size)
-                batch_data = X_train[offset:(offset + batch_size), :]
-                batch_labels = y_train[offset:(offset + batch_size), :]
-
-                if i % 100 == 0:
-                    train_accuracy = accuracy.eval(feed_dict={
-                        x: batch_data, y_: batch_labels, keep_prob: 1.0})
-                    print('step {}, training accuracy {}'.format(i, train_accuracy))
-                train_step.run(feed_dict={x: batch_data, y_: batch_labels, keep_prob: 0.5})
+            trainer.run_training()
             saver.save(sess, 'model/mnist_model_2')
-
-            print('test accuracy %g' % accuracy.eval(feed_dict={
-                x: X_val[:1000], y_: y_val[:1000], keep_prob: 1.0}))
 
     else:
         with tf.Session() as sess:
             saver.restore(sess, tf.train.latest_checkpoint('model/'))
             img = convert_img(cv2.imread("/mnt/remote/extracted_fields/fs10061402175155/10_27fs10061402175155.jpg", 0))
-            feed_dict = {x: img, keep_prob: 1.0}
+
+            y_conv, keep_prob = trainer.convDeepnn(trainer.x)
+            trainer.initialize_tensors(y_conv)
+
+
+            feed_dict = {trainer.x: img, keep_prob: 1.0}
             # feed_dict = {x: img, y_: np.zeros([1, 1000], dtype=np.int64), keep_prob: 1.0}
-            print(sess.run(predictor, feed_dict))
+            print(sess.run(trainer.predictor, feed_dict))
             # print(sess.run(correct_prediction, feed_dict))
-            print(sess.run(predictor2, feed_dict))
+            print(sess.run(trainer.predictor2, feed_dict))
             # print(sess.run(predictor, feed_dict={x: [X_test[0]], keep_prob: 1.0}))
             # cv2.imshow("1", X_test[0])
             # cv2.imshow("", img[0])
