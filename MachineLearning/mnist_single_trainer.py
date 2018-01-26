e4df5d95ec97336a9506339b9243e16f145f1266
tteige <tim.teige91@gmail.com>
2018-01-26 09:50:37 +0100

    returned to original mnist

--- MachineLearning/mnist_single_trainer.py
+++ MachineLearning/mnist_single_trainer.py
@@ -3,35 +3,20 @@ import h5py
 import argparse
 import numpy as np
 import cv2
+from tensorflow.examples.tutorials.mnist import input_data
 
 
 class Trainer:
     def __init__(self):
 
-        self.X_train = None
-        self.y_train = None
-        self.y_val = None
-        self.X_val = None
-        self.X_test = None
-        self.y_test = None
-        self.x = tf.placeholder(tf.float32, [None, 128, 128], name='x')
-        self.y_ = tf.placeholder(tf.int64, [None, 1001], name='y_')
+        self.x = tf.placeholder(tf.float32, [None, 784], name='x')
+        self.y_ = tf.placeholder(tf.int64, [None, 10], name='y_')
 
         self.batch_size = 50
         self.predictor = None
         self.predictor2 = None
         self.y_conv = None
         self.keep_prob = 1.0
-
-    def load_training_dataset(self, path='data/MNIST_synthetic.h5'):
-        h5f = h5py.File(path, 'r')
-
-        self.X_train = h5f['train_dataset'][:]
-        self.y_train = h5f['train_labels'][:]
-        self.y_val = h5f['valid_labels'][:]
-        self.X_val = h5f['valid_dataset'][:]
-        self.X_test = h5f['test_dataset'][:]
-        self.y_test = h5f['test_labels'][:]
 
     def weight_variable(self, shape):
         initial = tf.truncated_normal(shape, stddev=0.1)
@@ -52,7 +37,7 @@ class Trainer:
         W_conv1 = self.weight_variable([5, 5, 1, 32])
         b_conv1 = self.bias_variable([32])
 
-        x_image = tf.reshape(x, [-1, 128, 128, 1])
+        x_image = tf.reshape(x, [-1, 28, 28, 1])
 
         h_conv1 = tf.nn.relu(self.conv2d(x_image, W_conv1) + b_conv1)
         h_pool1 = self.max_pool_2x2(h_conv1)
@@ -63,25 +48,24 @@ class Trainer:
         h_conv2 = tf.nn.relu(self.conv2d(h_pool1, W_conv2) + b_conv2)
         h_pool2 = self.max_pool_2x2(h_conv2)
 
-        W_fc1 = self.weight_variable([32 * 32 * 64, 1024])
+        W_fc1 = self.weight_variable([7 * 7 * 64, 1024])
         b_fc1 = self.bias_variable([1024])
 
-        h_pool2_flat = tf.reshape(h_pool2, [-1, 32 * 32 * 64])
+        h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
         h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
         keep_prob = tf.placeholder(tf.float32)
         h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
-        W_fc2 = self.weight_variable([1024, 1001])
-        b_fc2 = self.bias_variable([1001])
+        W_fc2 = self.weight_variable([1024, 10])
+        b_fc2 = self.bias_variable([10])
 
         y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
 
         return y_conv, keep_prob
 
     def run_training(self):
-
+        mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
         y_conv, keep_prob = self.convDeepnn(self.x)
-        self.load_training_dataset()
 
         cross_entropy = tf.reduce_mean(
             tf.nn.softmax_cross_entropy_with_logits(labels=self.y_, logits=y_conv))
@@ -91,45 +75,43 @@ class Trainer:
         accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
         for i in range(200000):
-            offset = (i * self.batch_size) % (self.y_train.shape[0] - self.batch_size)
-            batch_data = self.X_train[offset:(offset + self.batch_size), :]
-            batch_labels = self.y_train[offset:(offset + self.batch_size), :]
-
+            batch = mnist.train.next_batch(50)
             if i % 100 == 0:
                 train_accuracy = accuracy.eval(feed_dict={
-                    self.x: batch_data, self.y_: batch_labels, keep_prob: 1.0})
+                    self.x: batch[0], self.y_: batch[1], keep_prob: self.keep_prob})
 
                 print('step {}, training accuracy {}'.format(i, train_accuracy))
 
-            train_step.run(feed_dict={self.x: batch_data, self.y_: batch_labels, keep_prob: 0.5})
+            train_step.run(feed_dict={self.x: batch[0], self.y_: batch[1], keep_prob: 0.5})
 
         print('test accuracy %g' % accuracy.eval(feed_dict={
-            self.x: self.X_val[:1000], self.y_: self.y_val[:1000], keep_prob: 1.0}))
+            self.x: mnist.test.images, self.y_: mnist.test.labels, keep_prob: 1.0}))
 
     def initialize_tensors(self, y_conv):
         self.predictor = tf.nn.softmax(y_conv, 1, name='predictor')
         self.predictor2 = tf.argmax(y_conv, 1)
 
 
+# Use this to normalize and flatten the image
 def convert_img(img):
-    img_data = cv2.bitwise_not(img)
+    # img_data = cv2.bitwise_not(img)
     # img_data = np.array(img_data, dtype=np.float32)
     # img_data = img_data.flatten()
     # img_data = [float(x) * 1.0 / 255.0 for x in img_data]
     # img_data = np.reshape(img_data, [64, 64]).astype(np.float32)
 
-    gray_channel = cv2.resize(img_data, (84, 28), interpolation=cv2.INTER_CUBIC)
-
-    reshaped = np.zeros((128, 128))
-    p = np.array(gray_channel)
-    y_off = 50
-    x_off = 22
-    reshaped[y_off:p.shape[0] + y_off, x_off:p.shape[1] + x_off] = p
+    # gray_channel = cv2.resize(img_data, (28, 28), interpolation=cv2.INTER_CUBIC)
+    #
+    # reshaped = np.zeros((28, 28))
+    # p = np.array(gray_channel)
+    # y_off = 50
+    # x_off = 22
+    # reshaped[y_off:p.shape[0] + y_off, x_off:p.shape[1] + x_off] = p
     # reshaped = cv2.blur(reshaped, (1,1))
-    img_data = [reshaped]
+    # img_data = [reshaped]
     # img_data = np.expand_dims(img_data, axis=3)
     # img_data = img_data.tolist()
-    return img_data
+    return img
 
 
 def run(args):
