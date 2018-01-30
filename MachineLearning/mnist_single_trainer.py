372495265349d761d434f3fa2ea0081baff0a3fd
tteige <tim.teige91@gmail.com>
2018-01-30 13:09:56 +0100

    Now using google mnist advanced for training

--- MachineLearning/mnist_single_trainer.py
+++ MachineLearning/mnist_single_trainer.py
@@ -9,223 +9,102 @@ from __future__ import print_function
 import argparse
 import sys
 import tempfile
+
+import os
+
+import cv2
 from tensorflow.examples.tutorials.mnist import input_data
 
 import tensorflow as tf
 
 
-# import tensorflow as tf
-# import h5py
-# import argparse
-# import numpy as np
-# import cv2
-# from tensorflow.examples.tutorials.mnist import input_data
-#
-#
-# class Trainer:
-#     def __init__(self):
-#         self.batch_size = 50
-#         self.predictor = None
-#         self.predictor2 = None
-#         self.y_conv = None
-#         self.keep_prob = 1.0
-#
-#     @staticmethod
-#     def weight_variable(shape):
-#         initial = tf.truncated_normal(shape, stddev=0.1)
-#         return tf.Variable(initial)
-#
-#     @staticmethod
-#     def bias_variable(shape):
-#         initial = tf.constant(0.1, shape=shape)
-#         return tf.Variable(initial)
-#
-#     @staticmethod
-#     def conv2d(x, W):
-#         return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
-#
-#     @staticmethod
-#     def max_pool_2x2(x):
-#         return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
-#                               strides=[1, 2, 2, 1], padding='SAME')
-#
-#     def convDeepnn(self, x):
-#         W_conv1 = self.weight_variable([5, 5, 1, 32])
-#         b_conv1 = self.bias_variable([32])
-#
-#         x_image = tf.reshape(x, [-1, 28, 28, 1])
-#
-#         h_conv1 = tf.nn.relu(self.conv2d(x_image, W_conv1) + b_conv1)
-#         h_pool1 = self.max_pool_2x2(h_conv1)
-#
-#         W_conv2 = self.weight_variable([5, 5, 32, 64])
-#         b_conv2 = self.bias_variable([64])
-#
-#         h_conv2 = tf.nn.relu(self.conv2d(h_pool1, W_conv2) + b_conv2)
-#         h_pool2 = self.max_pool_2x2(h_conv2)
-#
-#         W_fc1 = self.weight_variable([7 * 7 * 64, 1024])
-#         b_fc1 = self.bias_variable([1024])
-#
-#         h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
-#         h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
-#
-#         keep_prob = tf.placeholder(tf.float32)
-#         h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
-#         W_fc2 = self.weight_variable([1024, 10])
-#         b_fc2 = self.bias_variable([10])
-#
-#         y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
-#
-#         return y_conv, keep_prob
-#
-#     def run_training(self):
-#         with tf.Session() as sess:
-#             sess.run(tf.global_variables_initializer())
-#             x = tf.placeholder(tf.float32, [None, 784], name='x')
-#             y_ = tf.placeholder(tf.float32, [None, 10], name='y_')
-#
-#             mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
-#             y_conv, keep_prob = self.convDeepnn(x)
-#
-#             cross_entropy = tf.reduce_mean(
-#                 tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
-#             train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-#
-#             correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1), name='cor_pred')
-#             accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-#             saver = tf.train.Saver()
-#             if tf.train.latest_checkpoint("model/"):
-#                 saver.restore(sess, tf.train.latest_checkpoint('model/'))
-#
-#             for i in range(200000):
-#                 batch = mnist.train.next_batch(50)
-#                 if i % 100 == 0:
-#                     train_accuracy = accuracy.eval(feed_dict={
-#                         x: batch[0], y_: batch[1], keep_prob: self.keep_prob})
-#
-#                     print('step {}, training accuracy {}'.format(i, train_accuracy))
-#
-#                 train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
-#
-#             print('test accuracy %g' % accuracy.eval(feed_dict={
-#                 x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
-#
-#             saver.save(sess, 'model/mnist_model_2')
-#
-#     def initialize_tensors(self, y_conv):
-#         self.predictor = tf.nn.softmax(y_conv, 1, name='predictor')
-#         self.predictor2 = tf.argmax(y_conv, 1)
-#
-#
-# # Use this to normalize and flatten the image
-# def convert_img(img):
-#     # img_data = cv2.bitwise_not(img)
-#     # img_data = np.array(img_data, dtype=np.float32)
-#     # img_data = img_data.flatten()
-#     # img_data = [float(x) * 1.0 / 255.0 for x in img_data]
-#     # img_data = np.reshape(img_data, [64, 64]).astype(np.float32)
-#
-#     # gray_channel = cv2.resize(img_data, (28, 28), interpolation=cv2.INTER_CUBIC)
-#     #
-#     # reshaped = np.zeros((28, 28))
-#     # p = np.array(gray_channel)
-#     # y_off = 50
-#     # x_off = 22
-#     # reshaped[y_off:p.shape[0] + y_off, x_off:p.shape[1] + x_off] = p
-#     # reshaped = cv2.blur(reshaped, (1,1))
-#     # img_data = [reshaped]
-#     # img_data = np.expand_dims(img_data, axis=3)
-#     # img_data = img_data.tolist()
-#     return img
+def deepnn(x):
+    """deepnn builds the graph for a deep net for classifying digits.
+    Args:
+      x: an input tensor with the dimensions (N_examples, 784), where 784 is the
+      number of pixels in a standard MNIST image.
+    Returns:
+      A tuple (y, keep_prob). y is a tensor of shape (N_examples, 10), with values
+      equal to the logits of classifying the digit into one of 10 classes (the
+      digits 0-9). keep_prob is a scalar placeholder for the probability of
+      dropout.
+    """
+    # Reshape to use within a convolutional neural net.
+    # Last dimension is for "features" - there is only one here, since images are
+    # grayscale -- it would be 3 for an RGB image, 4 for RGBA, etc.
+    with tf.name_scope('reshape'):
+        x_image = tf.reshape(x, [-1, 28, 28, 1])
+
+    # First convolutional layer - maps one grayscale image to 32 feature maps.
+    with tf.name_scope('conv1'):
+        W_conv1 = weight_variable([5, 5, 1, 32])
+        b_conv1 = bias_variable([32])
+        h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+
+    # Pooling layer - downsamples by 2X.
+    with tf.name_scope('pool1'):
+        h_pool1 = max_pool_2x2(h_conv1)
+
+    # Second convolutional layer -- maps 32 feature maps to 64.
+    with tf.name_scope('conv2'):
+        W_conv2 = weight_variable([5, 5, 32, 64])
+        b_conv2 = bias_variable([64])
+        h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+
+    # Second pooling layer.
+    with tf.name_scope('pool2'):
+        h_pool2 = max_pool_2x2(h_conv2)
+
+    # Fully connected layer 1 -- after 2 round of downsampling, our 28x28 image
+    # is down to 7x7x64 feature maps -- maps this to 1024 features.
+    with tf.name_scope('fc1'):
+        W_fc1 = weight_variable([7 * 7 * 64, 1024])
+        b_fc1 = bias_variable([1024])
+
+        h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
+        h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
+    # Dropout - controls the complexity of the model, prevents co-adaptation of
+    # features.
+    with tf.name_scope('dropout'):
+        keep_prob = tf.placeholder(tf.float32)
+        h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+    # Map the 1024 features to 10 classes, one for each digit
+    with tf.name_scope('fc2'):
+        W_fc2 = weight_variable([1024, 10])
+        b_fc2 = bias_variable([10])
+
+        y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+    return y_conv, keep_prob
+
+
+def conv2d(x, W):
+    """conv2d returns a 2d convolution layer with full stride."""
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+
+def max_pool_2x2(x):
+    """max_pool_2x2 downsamples a feature map by 2X."""
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
+                          strides=[1, 2, 2, 1], padding='SAME')
+
+
+def weight_variable(shape):
+    """weight_variable generates a weight variable of a given shape."""
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
+
+
+def bias_variable(shape):
+    """bias_variable generates a bias variable of a given shape."""
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
 
 
 def run(args):
-    # Disable linter warnings to maintain consistency with tutorial.
-    # pylint: disable=invalid-name
-    # pylint: disable=g-bad-import-order
-    FLAGS = None
-
-    def deepnn(x):
-        """deepnn builds the graph for a deep net for classifying digits.
-        Args:
-          x: an input tensor with the dimensions (N_examples, 784), where 784 is the
-          number of pixels in a standard MNIST image.
-        Returns:
-          A tuple (y, keep_prob). y is a tensor of shape (N_examples, 10), with values
-          equal to the logits of classifying the digit into one of 10 classes (the
-          digits 0-9). keep_prob is a scalar placeholder for the probability of
-          dropout.
-        """
-        # Reshape to use within a convolutional neural net.
-        # Last dimension is for "features" - there is only one here, since images are
-        # grayscale -- it would be 3 for an RGB image, 4 for RGBA, etc.
-        with tf.name_scope('reshape'):
-            x_image = tf.reshape(x, [-1, 28, 28, 1])
-
-        # First convolutional layer - maps one grayscale image to 32 feature maps.
-        with tf.name_scope('conv1'):
-            W_conv1 = weight_variable([5, 5, 1, 32])
-            b_conv1 = bias_variable([32])
-            h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-
-        # Pooling layer - downsamples by 2X.
-        with tf.name_scope('pool1'):
-            h_pool1 = max_pool_2x2(h_conv1)
-
-        # Second convolutional layer -- maps 32 feature maps to 64.
-        with tf.name_scope('conv2'):
-            W_conv2 = weight_variable([5, 5, 32, 64])
-            b_conv2 = bias_variable([64])
-            h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-
-        # Second pooling layer.
-        with tf.name_scope('pool2'):
-            h_pool2 = max_pool_2x2(h_conv2)
-
-        # Fully connected layer 1 -- after 2 round of downsampling, our 28x28 image
-        # is down to 7x7x64 feature maps -- maps this to 1024 features.
-        with tf.name_scope('fc1'):
-            W_fc1 = weight_variable([7 * 7 * 64, 1024])
-            b_fc1 = bias_variable([1024])
-
-            h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
-            h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
-
-        # Dropout - controls the complexity of the model, prevents co-adaptation of
-        # features.
-        with tf.name_scope('dropout'):
-            keep_prob = tf.placeholder(tf.float32)
-            h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
-
-        # Map the 1024 features to 10 classes, one for each digit
-        with tf.name_scope('fc2'):
-            W_fc2 = weight_variable([1024, 10])
-            b_fc2 = bias_variable([10])
-
-            y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
-        return y_conv, keep_prob
-
-    def conv2d(x, W):
-        """conv2d returns a 2d convolution layer with full stride."""
-        return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
-
-    def max_pool_2x2(x):
-        """max_pool_2x2 downsamples a feature map by 2X."""
-        return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
-                              strides=[1, 2, 2, 1], padding='SAME')
-
-    def weight_variable(shape):
-        """weight_variable generates a weight variable of a given shape."""
-        initial = tf.truncated_normal(shape, stddev=0.1)
-        return tf.Variable(initial)
-
-    def bias_variable(shape):
-        """bias_variable generates a bias variable of a given shape."""
-        initial = tf.constant(0.1, shape=shape)
-        return tf.Variable(initial)
-
-    def main(_):
+    if args.train:
+        FLAGS = None
         # Import data
         mnist = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
 
@@ -258,6 +137,11 @@ def run(args):
 
         with tf.Session() as sess:
             sess.run(tf.global_variables_initializer())
+
+            saver = tf.train.Saver()
+            if tf.train.latest_checkpoint("model/"):
+                saver.restore(sess, tf.train.latest_checkpoint('model/'))
+
             for i in range(20000):
                 batch = mnist.train.next_batch(50)
                 if i % 100 == 0:
@@ -269,6 +153,21 @@ def run(args):
             print('test accuracy %g' % accuracy.eval(feed_dict={
                 x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
 
+            if not os.path.exists('model'):
+                os.mkdir('model')
+
+            saver.save(sess, 'model/mnist_model_2')
+
+    else:
+        with tf.Session() as sess:
+            saver = tf.train.Saver()
+            saver.restore(sess, tf.train.latest_checkpoint('model/'))
+            img = cv2.imread("/mnt/remote/extracted_fields/fs10061402175155/10_27fs10061402175155.jpg", 0)
+
+            x = tf.placeholder(tf.float32, [None, 784])
+            y_conv, keep_prob = deepnn(x)
+
+
     if __name__ == '__main__':
         parser = argparse.ArgumentParser()
         parser.add_argument('--data_dir', type=str,
@@ -276,106 +175,6 @@ def run(args):
                             help='Directory for storing input data')
         FLAGS, unparsed = parser.parse_known_args()
         tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
-    # trainer = Trainer()
-    #
-    # if args.train:
-    #     def weight_variable(shape):
-    #         initial = tf.truncated_normal(shape, stddev=0.1)
-    #         return tf.Variable(initial)
-    #
-    #     def bias_variable(shape):
-    #         initial = tf.constant(0.1, shape=shape)
-    #         return tf.Variable(initial)
-    #
-    #     def conv2d(x, W):
-    #         return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
-    #
-    #     def max_pool_2x2(x):
-    #         return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
-    #                               strides=[1, 2, 2, 1], padding='SAME')
-    #
-    #     def convDeepnn(x):
-    #         W_conv1 = weight_variable([5, 5, 1, 32])
-    #         b_conv1 = bias_variable([32])
-    #
-    #         x_image = tf.reshape(x, [-1, 28, 28, 1])
-    #
-    #         h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-    #         h_pool1 = max_pool_2x2(h_conv1)
-    #
-    #         W_conv2 = weight_variable([5, 5, 32, 64])
-    #         b_conv2 = bias_variable([64])
-    #
-    #         h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-    #         h_pool2 = max_pool_2x2(h_conv2)
-    #
-    #         W_fc1 = weight_variable([7 * 7 * 64, 1024])
-    #         b_fc1 = bias_variable([1024])
-    #
-    #         h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
-    #         h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
-    #
-    #         keep_prob = tf.placeholder(tf.float32)
-    #         h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
-    #         W_fc2 = weight_variable([1024, 10])
-    #         b_fc2 = bias_variable([10])
-    #
-    #         y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
-    #
-    #         return y_conv, keep_prob
-    #
-    #     with tf.Session() as sess:
-    #         sess.run(tf.global_variables_initializer())
-    #         x = tf.placeholder(tf.float32, [None, 784], name='x')
-    #         y_ = tf.placeholder(tf.float32, [None, 10], name='y_')
-    #
-    #         mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
-    #         y_conv, keep_prob = convDeepnn(x)
-    #
-    #         cross_entropy = tf.reduce_mean(
-    #             tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
-    #         train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-    #
-    #         correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1), name='cor_pred')
-    #         accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-    #         saver = tf.train.Saver()
-    #         if tf.train.latest_checkpoint("model/"):
-    #             saver.restore(sess, tf.train.latest_checkpoint('model/'))
-    #
-    #         for i in range(20000):
-    #             batch = mnist.train.next_batch(50)
-    #             if i % 100 == 0:
-    #                 train_accuracy = accuracy.eval(feed_dict={
-    #                     x: batch[0], y_: batch[1], keep_prob: 1.0})
-    #
-    #                 print('step {}, training accuracy {}'.format(i, train_accuracy))
-    #
-    #             train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
-    #
-    #         print('test accuracy %g' % accuracy.eval(feed_dict={
-    #             x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
-    #
-    #         saver.save(sess, 'model/mnist_model_2')
-
-    # else:
-    #     with tf.Session() as sess:
-    #         saver = tf.train.Saver()
-    #         saver.restore(sess, tf.train.latest_checkpoint('model/'))
-    #         img = convert_img(cv2.imread("/mnt/remote/extracted_fields/fs10061402175155/10_27fs10061402175155.jpg", 0))
-    #
-    #         y_conv, keep_prob = trainer.convDeepnn(trainer.x)
-    #         trainer.initialize_tensors(y_conv)
-    #
-    #
-    #         feed_dict = {trainer.x: img, keep_prob: 1.0}
-    #         # feed_dict = {x: img, y_: np.zeros([1, 1000], dtype=np.int64), keep_prob: 1.0}
-    #         print(sess.run(trainer.predictor, feed_dict))
-    #         # print(sess.run(correct_prediction, feed_dict))
-    #         print(sess.run(trainer.predictor2, feed_dict))
-    #         # print(sess.run(predictor, feed_dict={x: [X_test[0]], keep_prob: 1.0}))
-    #         # cv2.imshow("1", X_test[0])
-    #         # cv2.imshow("", img[0])
-    #         # cv2.waitKey()
 
 
 def main():
