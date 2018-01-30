c1fb350a21076d59fa05866967e6e9e83ca2c952
tteige <tim.teige91@gmail.com>
2018-01-30 11:38:43 +0100

    Tried to fix variable 1 op read bug

--- MachineLearning/mnist_single_trainer.py
+++ MachineLearning/mnist_single_trainer.py
@@ -14,18 +14,22 @@ class Trainer:
         self.y_conv = None
         self.keep_prob = 1.0
 
-    def weight_variable(self, shape):
+    @staticmethod
+    def weight_variable(shape):
         initial = tf.truncated_normal(shape, stddev=0.1)
         return tf.Variable(initial)
 
-    def bias_variable(self, shape):
+    @staticmethod
+    def bias_variable(shape):
         initial = tf.constant(0.1, shape=shape)
         return tf.Variable(initial)
 
-    def conv2d(self, x, W):
+    @staticmethod
+    def conv2d(x, W):
         return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
-    def max_pool_2x2(self, x):
+    @staticmethod
+    def max_pool_2x2(x):
         return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
                               strides=[1, 2, 2, 1], padding='SAME')
 
@@ -59,38 +63,39 @@ class Trainer:
 
         return y_conv, keep_prob
 
-    def run_training(self, sess):
+    def run_training(self):
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            x = tf.placeholder(tf.float32, [None, 784], name='x')
+            y_ = tf.placeholder(tf.float32, [None, 10], name='y_')
 
-        x = tf.placeholder(tf.float32, [None, 784], name='x')
-        y_ = tf.placeholder(tf.int64, [None, 10], name='y_')
+            mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+            y_conv, keep_prob = self.convDeepnn(x)
 
-        mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
-        y_conv, keep_prob = self.convDeepnn(x)
+            cross_entropy = tf.reduce_mean(
+                tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
+            train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 
-        cross_entropy = tf.reduce_mean(
-            tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
-        train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+            correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1), name='cor_pred')
+            accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+            saver = tf.train.Saver()
+            if tf.train.latest_checkpoint("model/"):
+                saver.restore(sess, tf.train.latest_checkpoint('model/'))
 
-        correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1), name='cor_pred')
-        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-        saver = tf.train.Saver()
-        if tf.train.latest_checkpoint("model/"):
-            saver.restore(sess, tf.train.latest_checkpoint('model/'))
+            for i in range(200000):
+                batch = mnist.train.next_batch(50)
+                if i % 100 == 0:
+                    train_accuracy = accuracy.eval(feed_dict={
+                        x: batch[0], y_: batch[1], keep_prob: self.keep_prob})
 
-        for i in range(200000):
-            batch = mnist.train.next_batch(50)
-            if i % 100 == 0:
-                train_accuracy = accuracy.eval(feed_dict={
-                    x: batch[0], y_: batch[1], keep_prob: self.keep_prob})
+                    print('step {}, training accuracy {}'.format(i, train_accuracy))
 
-                print('step {}, training accuracy {}'.format(i, train_accuracy))
+                train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
-            train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+            print('test accuracy %g' % accuracy.eval(feed_dict={
+                x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
 
-        print('test accuracy %g' % accuracy.eval(feed_dict={
-            x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
-
-        saver.save(sess, 'model/mnist_model_2')
+            saver.save(sess, 'model/mnist_model_2')
 
     def initialize_tensors(self, y_conv):
         self.predictor = tf.nn.softmax(y_conv, 1, name='predictor')
@@ -124,9 +129,7 @@ def run(args):
 
     if args.train:
 
-        with tf.Session() as sess:
-            sess.run(tf.global_variables_initializer())
-            trainer.run_training(sess)
+        trainer.run_training()
 
     else:
         with tf.Session() as sess:
