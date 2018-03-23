40852320ae6db3cfe7426a880c94af39a3860668
tteige <tim.teige91@gmail.com>
2018-03-23 16:58:35 +0100

    Added a database and an initialization script

---
+++ Database/dbHandler.py
@@ -0,0 +1,46 @@
+import sqlite3
+
+
+class DbHandler:
+    def __init__(self, db_loc, connection=None):
+        self.db_loc = db_loc
+        self.connection = connection
+        self.cursor = None
+        if self.connection is None:
+            self.connection = sqlite3.connect(db_loc)
+        if self.connection is not None:
+            self.cursor = self.connection.cursor()
+
+    def store_field(self, name, img):
+        self.cursor.execute('insert into fields VALUES (?, ?, ?, ?)', (name, img, img.shape[0], img.shape[1]))
+
+    def store_digit(self, name, img):
+        self.cursor.execute('insert into digit VALUES (?, ?)', (name, img))
+
+    def store_dropped(self, name, reason):
+        self.cursor.execute('insert into dropped VALUES (?, ?)', (name, reason))
+
+    def test_exists(self, name):
+        self.cursor.execute("SELECT EXISTS(SELECT 1 FROM fields VALUES (?) LIMIT 1)", (name))
+        return self.cursor.fetchone()
+
+    def select_image(self, name):
+        self.cursor.execute("SELECT * FROM fields WHERE name=:name", {'name': name})
+        return self.cursor.fetchone()
+
+    def select_all_images(self):
+        return self.cursor.execute("SELECT * FROM fields")
+
+    def __enter__(self):
+        try:
+            if self.connection is None:
+                self.connection = sqlite3.connect(self.db_loc)
+                self.cursor = self.connection.cursor()
+            return self
+        except Exception as e:
+            print(e)
+            exit(1)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.connection.commit()
+        self.connection.close()
